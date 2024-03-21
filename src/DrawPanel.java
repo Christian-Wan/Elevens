c563dfa5e4f48f7bf244d38f3a31cbc77a960091
unknown <BT_4N2_27@BKTECH.LOCAL>
2024-03-21 14:00:03 -0400

    Initial commit

---
+++ src/DrawPanel.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
+import java.util.ArrayList;
+
+class DrawPanel extends JPanel implements MouseListener {
+
+    private ArrayList<Card> hand;
+    private Rectangle button;
+
+    public DrawPanel() {
+        button = new Rectangle(147, 300, 160, 26);
+        this.addMouseListener(this);
+        hand = Card.buildHand();
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int x = 120;
+        int y = 10;
+        int count = 0;
+        for (int i = 0; i < hand.size(); i++) {
+            Card c = hand.get(i);
+            if (c.getHighlight()) {
+                g.drawRect(x, y, c.getImage().getWidth(), c.getImage().getHeight());
+            }
+            c.setRectangleLocation(x, y);
+            g.drawImage(c.getImage(), x, y, null);
+            x = x + c.getImage().getWidth() + 10;
+            count++;
+            if (count % 3 == 0) {
+                y += 100;
+                x = 120;
+            }
+        }
+        g.setFont(new Font("Courier New", Font.BOLD, 20));
+        g.drawString("GET NEW CARDS", 150, 320);
+        g.drawRect((int)button.getX(), (int)button.getY(), (int)button.getWidth(), (int)button.getHeight());
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+        Point clicked = e.getPoint();
+
+        if (e.getButton() == 1) {
+            if (button.contains(clicked)) {
+                hand = Card.buildHand();
+            }
+
+            for (int i = 0; i < hand.size(); i++) {
+                Rectangle box = hand.get(i).getCardBox();
+                if (box.contains(clicked)) {
+                    hand.get(i).flipCard();
+                }
+            }
+        }
+
+        if (e.getButton() == 3) {
+            for (int i = 0; i < hand.size(); i++) {
+                Rectangle box = hand.get(i).getCardBox();
+                if (box.contains(clicked)) {
+                    hand.get(i).flipHighlight();
+                }
+            }
+        }
+
+
+    }
+
+    public void mouseReleased(MouseEvent e) { }
+    public void mouseEntered(MouseEvent e) { }
+    public void mouseExited(MouseEvent e) { }
+    public void mouseClicked(MouseEvent e) { }
+}
